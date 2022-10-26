d6adb297ca102d7fa42557c34ff9455b29d8dded
artitsamchooosri <33492904+artitsamchooosri@users.noreply.github.com>
2022-10-26 11:01:54 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 ob_start();
-ini_set('date.timezone','Asia/Manila');
-date_default_timezone_set('Asia/Manila');
+ini_set('date.timezone','Asia/Bangkok');
+date_default_timezone_set('Asia/Bangkok');
 session_start();
 
 require_once('initialize.php');
